e9789cefdf4a255e8adf162f32cfe9f450506d95
Piyush Khandelwal <piyushk@gmail.com>
2014-10-08 17:30:17 -0500

    more updates to an abstract domain

---
+++ include/bwi_rl/planning/domain.h
@@ -0,0 +1,23 @@
+#ifndef BWI_RL_PLANNING_DOMAIN_H
+#define BWI_RL_PLANNING_DOMAIN_H
+
+namespace bwi_rl {
+
+  class Domain {
+
+    public:
+
+      virtual bool initialize(Json::Value &experiment, const std::string &base_directory) = 0;
+      virtual void precomputeAndSavePolicy(int problem_identifier) = 0;
+      virtual void testInstance(int seed) = 0;
+      virtual ~Domain() {}
+
+    protected:
+
+      Domain() {}
+
+  };
+
+} /* bwi_rl */
+
+#endif /* end of include guard: BWI_RL_PLANNING_DOMAIN_H */
